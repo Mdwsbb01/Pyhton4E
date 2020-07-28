d66cf512abf605955cb4d87794236940c3e4aef6
Mdwsbb01 <Mdwsbb01@gmail.com>
2020-07-28 10:19:38 -0400

    chapter12_update

--- py4e_ex_chap12HTTP.py
+++ py4e_ex_chap12HTTP.py
@@ -8,7 +8,7 @@ mysock.send(cmd)
 
 while True:
     data = mysock.recv(512)
-    if (len(data) < 1):
+    if (len(data) < 1):  # judge the stream is closed or not
         break
     print(data.decode())
 mysock.close()
@@ -20,6 +20,25 @@ fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 for line in fhand:
     print(line.decode().rstrip())
 '''
+#html
+'''
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+#ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+prompt = 'Enter: '
+uname = input(prompt)
+html = urllib.request.urlopen(uname, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
+'''
 #Exercise 1: Change the socket program socket1.py
 # to prompt the user for the URL
 # so it can read any web page.
@@ -29,11 +48,30 @@ for line in fhand:
 # Add error checking using try and except to
 # handle the condition where the user
 # enters an improperly formatted or non-existent URL.
-import urllib.request, urllib.parse, urllib.error
-prompt = 'Enter the URL: '
-uname = input(prompt)
-fhand = urllib.request.urlopen(uname)
 
-for line in fhand:
-    line = line.decode().rstrip().split()
-    print(line)
+import socket
+
+while True:
+    url = input('Enter the url: ')
+
+    if url == 'done':
+        print('project end')
+        quit()
+
+    try:
+        hostname = url.split('/')[2]
+        mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        mysock.connect((hostname, 80))
+        cmd = ('GET '+url+' HTTP/1.0\r\n\r\n').encode() # no space
+        mysock.send(cmd)
+    except:
+        print('The invalid url')
+        continue
+
+    while True:
+        data = mysock.recv(512)
+        if len(data) < 1: # judge the stream is closed or not
+            break
+        print(data.decode(), end='')
+    mysock.close()
+
