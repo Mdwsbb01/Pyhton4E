ed3982ad9d5a5ce95461d1b5957303bbece954da
Mdwsbb01 <Mdwsbb01@gmail.com>
2020-07-30 10:30:09 -0400

    Chapter12_finish

--- py4e_ex_chap12HTTP.py
+++ py4e_ex_chap12HTTP.py
@@ -116,7 +116,7 @@ while True:
 # and (3) counting the overall number of characters in the document.
 # Don't worry about the headers for this exercise,
 # simply show the first 3000 characters of the document contents.
-
+'''
 import urllib.request, urllib.parse, urllib.error
 
 count = 0
@@ -150,5 +150,92 @@ while True:
                     print(words[:3000])
                     break
             print('The number of characters is: ', count) # can not print the real character number if the count > 3000
+'''
+#Exercise 4: Change the urllinks.py program to extract and count
+# paragraph (p) tags from the retrieved HTML document and
+# display the count of the paragraphs as the output of your program.
+# Do not display the paragraph text, only count them.
+# Test your program on several small web pages as well as
+# some larger web pages.
+'''
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
 
+#ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
+prompt = 'Enter the url: '
+uname = input(prompt)
+html = urllib.request.urlopen(uname, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+tags = soup('p')
+count = 0
+for tag in tags:
+    count = count + 1
+print(count)
+'''
+#Exercise 5: (Advanced) Change the socket program so that
+# it only shows data after the headers and a blank line
+# have been received. Remember that recv is receiving
+# characters (newlines and all), not lines.
+'''
+import socket
+
+while True:
+    url = input('Enter the url: ')
+
+    if url == 'done':
+        print('Project ended')
+        quit()
+
+    try:
+        hostname = url.split('/')[2]
+        mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        mysock.connect((hostname, 80))
+        cmd = ('GET '+url+' HTTP/1.0\r\n\r\n').encode() # no space
+        mysock.send(cmd)
+    except:
+        print('The invalid url')
+        continue
+
+    while True:
+        data = mysock.recv(3000)
+        if not data:
+            break
+        else:
+            data = data.decode()
+            header_end_pos = data.find('\r\n\r\n') + 4 # +4 means also ignore the space, can know it by checking the webpage sourcecode
+            refined_data = data[header_end_pos:]
+            if len(refined_data) < 1:
+                break
+            print(refined_data, end='')
+    mysock.close()
+'''
+#the example of github URL=https://stackoverflow.com/users/4952130/dimitris-fasarakis-hilliard
+import socket
+
+url = input('Enter the url: ')
+hostname = url.split('/')[2]
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect((hostname, 80))
+cmd = ('GET '+url+' HTTP/1.0\r\n\r\n').encode() # no space
+mysock.send(cmd)
+
+resp = []
+while True:
+    data = mysock.recv(3000)
+
+    if not data:
+        break
+    else:
+        resp.append(data.decode())
+mysock.close()
+
+resp = ''.join(resp)
+#print(resp)
+body = resp.partition('\r\n\r\n')[2] # using the assigned character to split
+print(body, end='')
